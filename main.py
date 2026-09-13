b2d8f6319f765a163224145c9d21854e3b92c9ea
Vallamalla Abhishek Prakash <176928169+vabhishekprakash@users.noreply.github.com>
2026-09-13 17:38:54 +0530

    changed time format to 12 hour clock with PM

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ root.resizable(False, False)
 root.configure(bg='white')
 
 def time():
-    string = strftime('%H:%M:%S %p')
+    string = strftime('%I:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)  
 
